99597c6c1f976c4456925f1087e626bf6f178a7f
RajpootDeepak <161629677+RajputDeepak69@users.noreply.github.com>
2026-07-13 07:08:04 +0530

    wrote backend logic for dashboard and inventory

---
+++ app/app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+import database
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+
+    stats = database.dashboard_stats()
+
+    servers = database.get_servers()
+
+    return render_template(
+
+        "dashboard.html",
+
+        stats=stats,
+
+        servers=servers[:5]
+
+    )
+
+@app.route("/inventory")
+def inventory():
+
+    servers = database.get_servers()
+
+    return render_template(
+
+        "inventory.html",
+
+        servers=servers
+
+    )
+
+@app.route("/add", methods=["GET","POST"])
+
+def add_server():
+
+    if request.method=="POST":
+
+        server={
+
+            "server_name":request.form["server_name"],
+            "environment":request.form["environment"],
+            "operating_system":request.form["operating_system"],
+            "ip_address":request.form["ip_address"],
+            "owner":request.form["owner"],
+            "status":request.form["status"]
+
+        }
+
+        database.add_server(server)
+
+        return redirect(url_for("inventory"))
+
+    return render_template("add_server.html")
+
+@app.route("/delete/<int:id>")
+
+def delete(id):
+
+    database.delete_server(id)
+
+    return redirect(url_for("inventory"))
+
+if __name__=="__main__":
+
+    app.run(
+
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+
+    )
